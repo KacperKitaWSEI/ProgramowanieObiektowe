5e6df70b975267b0f96ee1b7f56bf7f36401a943
Kacper Kita <kkita.contact@gmail.com>
2022-03-18 19:41:16 +0100

    Finish assignment

---
+++ Lab1/Fraction.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace Lab1
+{
+    public class Fraction : IEquatable<Fraction>, IComparable<Fraction>
+    {
+
+        //subsection 1
+        public int counter { get; private set; }//subsection 6
+        public int denominator { get; private set; }//subsection 6
+
+        public Fraction()
+        {
+
+        }
+
+        public Fraction(int a, int b)
+        {
+            counter = a;
+            denominator = b;
+        }
+
+        public Fraction(Fraction previousFraction)
+        {
+            counter = previousFraction.counter;
+            denominator = previousFraction.denominator;
+        }
+        //subsection 1 stop
+        //subsection 3 start
+        public static Fraction operator +(Fraction a) => a;
+        public static Fraction operator -(Fraction a) => new Fraction(-a.counter, a.denominator);
+
+        public static Fraction operator +(Fraction a, Fraction b)
+            => new Fraction(a.counter * b.denominator + b.counter * a.denominator, a.counter * b.denominator);
+
+        public static Fraction operator -(Fraction a, Fraction b)
+            => a + (-b);
+
+        public static Fraction operator *(Fraction a, Fraction b)
+            => new Fraction(a.counter * b.denominator, a.denominator * b.denominator);
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            if (b.counter == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return new Fraction(a.counter * b.denominator, a.denominator * b.counter);
+        }
+        //subsection 3 stop
+        //subsection 2 start
+        public override string ToString() => $"{counter} / {denominator}";
+        //subsection 2 stop
+        //subsection 4 start
+        public bool Equals(Fraction fraction)
+        {
+            return counter == fraction.counter && denominator == fraction.denominator;
+        }
+
+        public int CompareTo(Fraction other)
+        {
+            return (this - other).counter;
+        }
+        //subsection 4 stop
+        //subsection 6 start
+        public double RoundDown()
+        {
+            return Math.Floor((double)(counter / denominator));
+        }
+
+        public double RoundUp()
+        {
+            return Math.Ceiling((double)(counter / denominator));
+        }
+
+        //subsection 6 stop
+    }
+}
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -2,82 +2,16 @@
 
 namespace Lab1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Fraction fraction = new Fraction();
+            var fraction1 = new Fraction(1, 2);
+            var fraction2 = new Fraction(1, 3);
 
-            fraction.ToString();
-            Console.WriteLine(fraction.counter);
+            Console.WriteLine(fraction1 / fraction2);
         }
 
-        class Fraction : IEquatable<Fraction>
-        {
-            public int counter { get; private set; }
-            public int denominator { get; private set; }
-
-            public Fraction() 
-            {
-                counter = 20;
-                denominator = 45;
-            }
-
-            public Fraction(int a, int b)
-            {
-                counter = a;
-                counter = b;
-            }
-
-            public Fraction(Fraction previousFraction)
-            {
-                counter = previousFraction.counter;
-                denominator = previousFraction.denominator;
-            }
-
-            public void ToString()
-            {
-                Console.WriteLine($"{counter}/{denominator}");
-            }
-
-
-
-            public bool Equals(Fraction fraction)
-            {
-                if (fraction == null)
-                {
-                    return false;
-                }
-
-                if (this.counter == this.denominator)
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-
-    /*       public int CompareTo(object obj)
-            {
-                if (obj == null)
-                {
-                    return 0;
-                }
-
-                Fraction fraction = obj as Fraction;
-                if (fraction != null)
-                {
-                    return this.
-                }
-            }
-
-            public double Splitting(int a, int b)
-            {
-                return a / b;
-            }
-    */
-    }
+        
     }
 }
