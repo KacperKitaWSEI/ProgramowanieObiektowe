79eb14b7a6f8d5da9672fa2fda5f5f88c815590d
Kacper Kita Wsei <kacper.kita@microsoft.wsei.edu.pl>
2022-03-18 19:55:11 +0100

    Finish

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,10 +6,6 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            var fraction1 = new Fraction(1, 2);
-            var fraction2 = new Fraction(1, 3);
-
-            Console.WriteLine(fraction1 / fraction2);
         }
 
         
