0de9a2ae69fd93ef83ad7cc9cd630f7ca2201bd8
unknown <Kacper.Kita@microsoft.wsei.edu.pl>
2022-05-07 15:43:27 +0200

    Labs

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ObservableList1 list = new ObservableList1();
+
+        list.Added += (object sender, ChangedEventArgs e) =>
+        {
+            Console.WriteLine("1: Added: value=" + e.Value);
+        };
+
+        list.Add(+1);
+    }
+}
+
+public class ObservableList1
+{
+    private List<int> list = new List<int>();
+
+    public event EventHandler<ChangedEventArgs> Added;
+
+    protected virtual void OnAdded(ChangedEventArgs e)
+    {
+        EventHandler<ChangedEventArgs> handler = this.Added;
+
+        if (handler != null)
+            handler(this, e);
+    }
+
+    public void Add(int value)
+    {
+        this.list.Add(value);
+        this.OnAdded(new ChangedEventArgs(value));
+    }
+}
+
+public class ChangedEventArgs : EventArgs
+{
+    public int Value { get; set; }
+
+    public ChangedEventArgs(int value)
+    {
+        this.Value = value;
+    }
+}
